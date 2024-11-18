102428cd221a292fb2a44cd9dca6c02d624a0eb6
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-11-18 12:54:50 +0530

    added check-health endpoint

--- app.js
+++ app.js
@@ -7,9 +7,19 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
 app.get('/version', (req, res) => {
   res.send(1)
 })
+
+app.get('/version', (req, res) => {
+  res.send(1)
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   //eslint-disable-next-line
   console.log(`server started on port ${PORT}`)
