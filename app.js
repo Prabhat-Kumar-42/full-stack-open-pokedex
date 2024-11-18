4c151b516a4213259f81a1068dd797c3983f6866
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-11-18 08:18:28 +0530

    removed semicolons

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import process from 'process';
+import express from 'express'
+import process from 'process'
 
-const app = express();
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 app.get('/version', (req, res) => {
-  res.send(1);
-});
+  res.send(1)
+})
 app.listen(PORT, () => {
   //eslint-disable-next-line
-  console.log(`server started on port ${PORT}`);
-});
+  console.log(`server started on port ${PORT}`)
+})
